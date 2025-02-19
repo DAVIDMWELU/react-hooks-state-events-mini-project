b6d36b87aa8d9555f45fb691cd62eea6bfdee5cc
DAVIDMWELU <davidmwelu@gmail.com>
2025-02-20 01:34:19 +0300

    react hooks state events mini project

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, onCategoryChange }) {
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map(category => (
+        <button key={category} onClick={() => onCategoryChange(category)}>
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
